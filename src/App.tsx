a1c29e66f1346eae59b1ab3e11df379bdde084f2
unknown <nika.chichua26@gmail.com>
2024-08-22 16:54:28 +0400

    feat: scss setup

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ export default function App() {
   return (
     <>
       <h1>APP</h1>
+      <button>CLICK</button>
     </>
   );
 }
--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import "@src/assets/global.css";
+import "@src/assets/styles/global.scss";
 import App from "@src/App.tsx";
 
 createRoot(document.getElementById("root")!).render(
