d3b85d6773d2994e8d5fad6d5985e3b8b2bbff23
unknown <nika.chichua26@gmail.com>
2024-08-22 19:02:32 +0400

    chore: zod and react-hook-form installed

--- src/components/Form/Form.tsx
+++ src/components/Form/Form.tsx
@@ -3,9 +3,14 @@ import { PropsWithChildren } from "react";
 
 interface FormProps {
   title: string;
+  onSubmit: () => Promise<void>;
 }
 
-export function Form({ children, title }: PropsWithChildren<FormProps>) {
+export function Form({
+  children,
+  title,
+  onSubmit,
+}: PropsWithChildren<FormProps>) {
   return (
     <form className="form">
       <h1>{title}</h1>
