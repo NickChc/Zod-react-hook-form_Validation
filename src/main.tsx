1041320d6a894f166410f20ecb4a022f9c292eba
unknown <nika.chichua26@gmail.com>
2024-08-22 16:31:33 +0400

    chore: configured import alias @src

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,7 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import App from "./App.tsx";
+import "@src/assets/global.css";
+import App from "@src/App.tsx";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
