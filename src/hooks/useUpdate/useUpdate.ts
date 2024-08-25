25a4427c91943f30afc3bedc1c5984dbf0d8b81d
unknown <nika.chichua26@gmail.com>
2024-08-25 20:44:31 +0400

    feat: dynamic update shcemas

--- src/hooks/useUpdate/useUpdate.ts
+++ src/hooks/useUpdate/useUpdate.ts
@@ -1,15 +1,38 @@
 import { zodResolver } from "@hookform/resolvers/zod";
 import { TEditValue } from "@src/@types/general";
 import { useGlboalProvider } from "@src/providers/GlobalProvider";
+import { nameSchema, passwordSchema } from "@src/schemas/auth";
 import { useForm } from "react-hook-form";
 import { z } from "zod";
 
 export function useUpdate(editValue: TEditValue) {
   const { user } = useGlboalProvider();
 
-  const editSchema = z.object({
-    [editValue]: z.string().min(1, `Enter ${editValue} first`),
-  });
+  let editSchema;
+
+  switch (editValue) {
+    case "email":
+      editSchema = z.object({
+        [editValue]: z.string().min(1, "Empty field").email("Invalid email"),
+      });
+      break;
+    case "name":
+      editSchema = z.object({
+        [editValue]: nameSchema,
+      });
+      break;
+    case "password":
+      editSchema = z.object({
+        [editValue]: passwordSchema,
+      });
+      break;
+    case "birthday":
+    default:
+      editSchema = z.object({
+        [editValue]: z.string().min(1, `Enter ${editValue} first`),
+      });
+      break;
+  }
 
   const form = useForm({
     mode: "onSubmit",
